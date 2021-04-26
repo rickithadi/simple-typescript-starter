e076ad57ef9f91283be53fb5dd9cf1dcafc3e404
hadi <rickithadi@gmail.com>
2021-04-26 15:52:31 +0800

    created interfaces

---
+++ src/models.ts
@@ -0,0 +1,20 @@
+export interface Environment {
+  Id: string;
+  name: string;
+}
+export interface Deploy {
+  Id: string;
+  ReleaseId: string;
+  EnvironmentId: string;
+  DeployedAt: Date;
+}
+export interface Project {
+  Id: string;
+  name: string;
+}
+export interface Release {
+  Id: string;
+  ProjectId: string;
+  Version: string | null;
+  Created: string;
+}
