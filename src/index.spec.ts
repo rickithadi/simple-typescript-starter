59d269025cbf1bf8dc27563d2c57b66bb5675da8
hadi <rickithadi@gmail.com>
2021-04-26 16:01:23 +0800

    scaffolded files using seed project, imported data files and created models.

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,19 @@
+import releases from './data/Releases.json';
+import { checkReleaseDeployStatus } from './index';
+
 describe('test', () => {
   test('add', async () => {
     expect(1 + 1).toEqual(2);
   });
 });
+
+// describe('check deploy status of release', () => {
+//   it('deployed release returns true', () => {
+//     const result = checkReleaseDeployStatus(releases[0]);
+//     expect(result).toBe(true);
+//   });
+//   it('undeployed release returns false', () => {
+//     const result = checkReleaseDeployStatus(releases[1]);
+//     expect(result).toBe(false);
+//   });
+// });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('Hello world!');
+import { Environment, Deploy, Project, Release } from './models';
+import deployments from './data/Deployments.json';
+import environments from './data/Environments.json';
+import projects from './data/Projects.json';
+import releases from './data/Releases.json';
+
+export function checkReleaseDeployStatus(release: Release): boolean {
+  //search deployments for deployment.ReleaseId===release.Id
+  return false;
+}
+
+export function searchReleases(
+  project: Project,
+  environment: Environment,
+  releasesToKeep: number,
+) {
+  //look for corresponding deployed releases via
+  //
+  //sort by date
+  //
+  //trim
+}
